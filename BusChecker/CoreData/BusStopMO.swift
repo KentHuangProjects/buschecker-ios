e74eb4e3193a5559e66d8a2f51e9453334cd1dd4
Kent Huang <kenthuang426@gmail.com>
2017-10-20 00:51:30 -0700

    added gitignore

---
+++ BusChecker/CoreData/BusStopMO.swift
@@ -0,0 +1,9 @@
+//
+//  BusStopMO.swift
+//  BusChecker
+//
+//  Created by kenth on 2017-10-19.
+//  Copyright © 2017 kenth. All rights reserved.
+//
+
+import Foundation
---
+++ BusChecker/CoreData/MessageMO.swift
@@ -0,0 +1,9 @@
+//
+//  MessageMO.swift
+//  BusChecker
+//
+//  Created by kenth on 2017-10-19.
+//  Copyright © 2017 kenth. All rights reserved.
+//
+
+import Foundation
