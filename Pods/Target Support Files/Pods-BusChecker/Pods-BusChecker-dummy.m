c8f3ecf91615a1d3d780fa8e61601f92941194d2
Yu Hong Huang <kenthuang426@gmail.com>
2017-11-10 13:22:06 -0800

    added Pods

---
+++ Pods/Target Support Files/Pods-BusChecker/Pods-BusChecker-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BusChecker : NSObject
+@end
+@implementation PodsDummy_Pods_BusChecker
+@end
---
+++ Pods/Target Support Files/Pods-BusCheckerUITests/Pods-BusCheckerUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BusCheckerUITests : NSObject
+@end
+@implementation PodsDummy_Pods_BusCheckerUITests
+@end
